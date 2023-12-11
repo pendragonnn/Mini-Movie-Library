2bcb76a193ae02121986f5d0ad03d889ede74540
Wisnu Andika <wisnuandika104@gmail.com>
2023-12-11 19:30:34 +0700

    fix: change html tag in pagination

--- src/component/pagination/Pagination.jsx
+++ src/component/pagination/Pagination.jsx
@@ -5,13 +5,13 @@ export default function Pagination({ currentPage, totalPage, onPaginationNextHan
     <div className="container d-flex align-items-end flex-column mx-auto">
       <ul className="pagination">
         {currentPage !== 1 && (
-          <li className="page-item"><button className="page-link text-warning bg-secondary" onClick={() => onPaginationPreviousHandler(currentPage)}>Previous</button></li>
+          <li className="page-item"><a className="page-link text-warning bg-secondary" onClick={() => onPaginationPreviousHandler(currentPage)} href="#">Previous</a></li>
         )}
         <li className="page-item">
           <span className="page-link bg-dark text-warning">{currentPage}</span>
         </li>
         {currentPage !== totalPage && (
-          <li className="page-item"><button className="page-link text-warning bg-secondary" onClick={() => onPaginationNextHandler(currentPage)}>Next</button></li>
+          <li className="page-item"><a className="page-link text-warning bg-secondary" onClick={() => onPaginationNextHandler(currentPage)} href="#">Next</a></li>
         )}
       </ul>
       <small className="d-block">Page: {currentPage} / {totalPage}</small>
