21ba4280e374ee388c162c3911a083a13883018c
Wisnu Andika <wisnuandika104@gmail.com>
2023-12-12 10:50:36 +0700

    fix: change layout name

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "MovieMingle",
+  title: "Mini Movie Library",
   description: "Watch movies and enjoy popcorns at ease",
 };
 
